02661e31f39a135c7df50d15ef9c420c28afcb89
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-02 09:38:00 +0200

    Add project files.

---
+++ Services/ServiceCountries.cs
@@ -0,0 +1,15 @@
+﻿using ServiceCountriesNameSpace;
+
+namespace MvcCoreClienteWCF.Services
+{
+    public class ServiceCountries
+    {
+        CountryInfoServiceSoapTypeClient client;
+        public ServiceCountries()
+        {
+            this.client = new CountryInfoServiceSoapTypeClient
+                (CountryInfoServiceSoapTypeClient.EndpointConfiguration
+                .CountryInfoServiceSoap);
+        }
+    }
+}
