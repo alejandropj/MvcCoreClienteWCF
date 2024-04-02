b84f525f8dff38f923c47d4eb479babb63a9fc03
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-02 14:21:55 +0200

    02/04 v2

---
+++ Services/ServiceMetodosVarios.cs
@@ -0,0 +1,18 @@
+﻿using ServiceReferenceMetodosVarios;
+namespace MvcCoreClienteWCF.Services
+{
+    public class ServiceMetodosVarios
+    {
+        MetodosVariosContractClient client;
+        public ServiceMetodosVarios ( MetodosVariosContractClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<int[]> GetTablaMultiplicarAsync(int numero)
+        {
+            int[] datos=await this.client.GetTablaMultiplicarAsync(numero);
+            return datos;
+        }
+    }
+}
